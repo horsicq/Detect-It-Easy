29d3982431a578d6204429272ad10a705db1cae9
horsicq <horsicq@gmail.com>
2014-09-18 13:06:13 +0200

    Update diecl

--- diedll_win/SDK/diecl.c
+++ diedll_win/SDK/diecl.c
@@ -18,6 +18,12 @@
 #define PVERS "1.01"
 #define PDATE "3 August, 2014"
 
+#ifdef QT_VERSION
+#include <QtGlobal>
+#else
+#define Q_OS_WIN
+#endif
+
 #ifdef Q_OS_WIN
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
