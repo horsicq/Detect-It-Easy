27e4a6b1e3842522be493b9f7279bea2dd4537a9
horsicq <horsicq@gmail.com>
2015-04-04 02:05:58 +0200

    fix

--- diedll_win/SDK/simple.cpp
+++ diedll_win/SDK/simple.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char* argv[])
 {
-	char szBuffer[0x4000];
-	char *pszFileName="C:\\Windows\\notepad.exe";
-	
-	DIE_scanA(pszFileName,szBuffer,sizeof(szBuffer),DIE_SHOWOPTIONS+DIE_SHOWVERSION);
-	printf("%s",szBuffer);
+    char szBuffer[0x4000];
+    char *pszFileName="C:\\Windows\\notepad.exe";
 
-	return 0;
+    DIE_scanA(pszFileName,szBuffer,sizeof(szBuffer),DIE_SHOWOPTIONS+DIE_SHOWVERSION);
+    printf("%s",szBuffer);
+
+    return 0;
 }
 
