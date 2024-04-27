008e4a47d041dd0fd1d20ff094d7f03c19a9ebb1
horsicq <horsicq@gmail.com>
2024-04-27 21:52:56 +0200

    Update file(s): 2024-04-27

--- help/MACH.js
+++ help/MACH.js
@@ -2,241 +2,241 @@
  * @class
  * @classdesc This is a description of the MACH class.
  */
-function MACH(){}
+function MACH() {}
 /**
  * @see Binary.calculateEntropy
  */
-MACH.calculateEntropy=function(nOffset,nSize){}
-/**
- * @see Binary.calculateMD5
- */
-MACH.calculateMD5=function(nOffset,nSize){}
-/**
- * @see Binary.compare
- */
-MACH.compare=function(sSignature,nOffset){}
-/**
- * @see PE.compareEP
- */
-MACH.compareEP=function(sSignature,nOffset){}
-/**
- * @see Binary.findByte
- */
-MACH.findByte=function(nOffset,nSize,cValue){}
-/**
- * @see Binary.findDword
- */
-MACH.findDword=function(nOffset,nSize,nValue){}
-/**
- * @see Binary.findSignature
- */
-MACH.findSignature=function(nOffset,nSize,sSignature){}
-/**
- * @see Binary.findString
- */
-MACH.findString=function(nOffset,nSize,sValue){}
-/**
- * @see Binary.findWord
- */
-MACH.findWord=function(nOffset,nSize,sValue){}
-/**
- * @see Binary.getSignature
- */
-MACH.getSignature=function(nOffset,nSize){}
-/**
- * @see Binary.getSize
- */
-MACH.getSize=function(){}
-/**
- * @see Binary.isSignaturePresent
- */
-MACH.isSignaturePresent=function(nOffset,nSize,sSignature){}
-/**
- * @see Binary.readByte
- */
-MACH.readByte=function(nOffset){}
-/**
- * @see Binary.readDword
- */
-MACH.readDword=function(nOffset){}
-/**
- * @see Binary.readQword
- */
-MACH.readQword=function(nOffset){}
-/**
- * @see Binary.readWord
- */
-MACH.readWord=function(nOffset){}
-/**
- * @see Binary.readSByte
- */
-MACH.readSByte=function(nOffset){}
-/**
- * @see Binary.readSDword
- */
-MACH.readSDword=function(nOffset){}
-/**
- * @see Binary.readSQword
- */
-MACH.readSQword=function(nOffset){}
-/**
- * @see Binary.readSWord
- */
-MACH.readSWord=function(nOffset){}
-/**
- * @see Binary.swapBytes
- */
-MACH.swapBytes=function(nValue){}
-/**
- * @see Binary.getString
- */
-MACH.getString=function(nOffset,nSize){}
-/**
- * @see Binary.getFileDirectory
- */
-MACH.getFileDirectory=function(){}
-/**
- * Get a string in the form of <code>"<i>MACHtype</i><i>MACHmode</i>"</code>. For example <code>"EXE32"</code> or <code>"DYLIB64"</code>.
- * @returns {String}
- */
-MACH.getGeneralOptions=function(){}
-/**
- * Get the current version of a library.
- * @param {String} sLibrary - The name of the library.
- * @returns {String}
- */
-MACH.getLibraryCurrentVersion=function(sLibrary){}
-/**
- * Get the number of sections.
- * @returns {Int}
- */
-MACH.getNumberOfSections=function(){}
-/**
- * Get the number of segments.
- * @returns {Int}
- */
-MACH.getNumberOfSegments=function(){}
-/**
- * Check if there is a library with a specific name in the import table.
- * @param {String} sLibraryName - The name of the library.
- * @returns {Bool}
- */
-MACH.isLibraryPresent=function(sLibrary){}
-/**
- * Check if a section exists with a specific name or matches a regular expression.
- * @param {String} sSectionName - Section name or pattern.
- * @returns {Bool}
- */
-MACH.isSectionNamePresent=function(sSectionName){}
-/**
- * Get the number of a section with a specific name.
- * @param {String} sSectionName - Section name.
- * @returns {Int} 0-based section number, or <code>-1</code> if there is no section with that name.
- */
-MACH.getSectionNumber=function(sSectionName){}
-/**
- * Get the file offset of a section.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSectionFileOffset=function(nSection){}
-/**
- * Get the file size of a section.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSectionFileSize=function(nSection){}
-/**
- * Get the name of a section.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSectionName=function(nSection){}
-/**
- * Get the file offset of a segment.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSegmentFileOffset=function(nSection){}
-/**
- * Get the file size of a segment.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSegmentFileSize=function(nSection){}
-/**
- * Get the name of a segment.
- * @param {Int} nSection - Section number.
- * @returns {UInt}
- */
-MACH.getSegmentName=function(nSection){}
-/**
- * Check if the file is 64 bit (MachO64).
- * @returns {Bool}
- * @example
- * if(MACH.is64())
- * {
- *     sOptions="MachO64";
- * }
- */
-MACH.is64=function(){}
-/**
- * Convert a relative virtual address (RVA) to a file offset.
- * @param {UInt64} nRVA
- * @returns {Int64} If an error occurs, -1 will be returned.
- */
-MACH.RVAToOffset=function(nRVA){}
-/**
- * Convert a virtual address (VA) to a file offset.
- * @param {UInt64} nVA
- * @returns {Int64} If an error occurs, -1 will be returned.
- */
-MACH.VAToOffset=function(nVA){}
-/**
- * Convert a file offset to a relative virtual address (RVA).
- * @param {UInt64} nOffset
- * @returns {Int64} If an error occurs, -1 will be returned.
- */
-MACH.OffsetToRVA=function(nOffset){}
-/**
- * Convert a file offset to a virtual address (VA).
- * @param {UInt64} nOffset
- * @returns {Int64} If an error occurs, -1 will be returned.
- */
-MACH.OffsetToVA=function(nOffset){}
-/**
- * Get the size of the overlay.
- * @returns {Int}
- */
-MACH.getOverlaySize=function(){}
-/**
- * Get the file offset of the overlay.
- * @returns {Int}
- */
-MACH.getOverlayOffset=function(){}
-/**
- * Check if an overlay is present.
- * @returns {Bool}
- */
-MACH.isOverlayPresent=function(){}
-/**
- * @see PE.compareOverlay
- */
-MACH.compareOverlay=function(sSignature,nOffset){}
-/**
- * Get the offset of the entry point.
- * @returns {Int64} If an error occurs, -1 will be returned.
- */
-MACH.getEntryPointOffset=function(){}
-/**
- * @see Binary.getFileBaseName
- */
-MACH.getFileBaseName=function(){}
-/**
- * @see Binary.getFileSuffix
- */
-MACH.getFileSuffix=function(){}
-/**
- * @see Binary.getFileCompleteSuffix
- */
-MACH.getFileCompleteSuffix=function(){}
+MACH.calculateEntropy = function(nOffset, nSize) {}
+    /**
+     * @see Binary.calculateMD5
+     */
+MACH.calculateMD5 = function(nOffset, nSize) {}
+    /**
+     * @see Binary.compare
+     */
+MACH.compare = function(sSignature, nOffset) {}
+    /**
+     * @see PE.compareEP
+     */
+MACH.compareEP = function(sSignature, nOffset) {}
+    /**
+     * @see Binary.findByte
+     */
+MACH.findByte = function(nOffset, nSize, cValue) {}
+    /**
+     * @see Binary.findDword
+     */
+MACH.findDword = function(nOffset, nSize, nValue) {}
+    /**
+     * @see Binary.findSignature
+     */
+MACH.findSignature = function(nOffset, nSize, sSignature) {}
+    /**
+     * @see Binary.findString
+     */
+MACH.findString = function(nOffset, nSize, sValue) {}
+    /**
+     * @see Binary.findWord
+     */
+MACH.findWord = function(nOffset, nSize, sValue) {}
+    /**
+     * @see Binary.getSignature
+     */
+MACH.getSignature = function(nOffset, nSize) {}
+    /**
+     * @see Binary.getSize
+     */
+MACH.getSize = function() {}
+    /**
+     * @see Binary.isSignaturePresent
+     */
+MACH.isSignaturePresent = function(nOffset, nSize, sSignature) {}
+    /**
+     * @see Binary.readByte
+     */
+MACH.readByte = function(nOffset) {}
+    /**
+     * @see Binary.readDword
+     */
+MACH.readDword = function(nOffset) {}
+    /**
+     * @see Binary.readQword
+     */
+MACH.readQword = function(nOffset) {}
+    /**
+     * @see Binary.readWord
+     */
+MACH.readWord = function(nOffset) {}
+    /**
+     * @see Binary.readSByte
+     */
+MACH.readSByte = function(nOffset) {}
+    /**
+     * @see Binary.readSDword
+     */
+MACH.readSDword = function(nOffset) {}
+    /**
+     * @see Binary.readSQword
+     */
+MACH.readSQword = function(nOffset) {}
+    /**
+     * @see Binary.readSWord
+     */
+MACH.readSWord = function(nOffset) {}
+    /**
+     * @see Binary.swapBytes
+     */
+MACH.swapBytes = function(nValue) {}
+    /**
+     * @see Binary.getString
+     */
+MACH.getString = function(nOffset, nSize) {}
+    /**
+     * @see Binary.getFileDirectory
+     */
+MACH.getFileDirectory = function() {}
+    /**
+     * Get a string in the form of <code>"<i>MACHtype</i><i>MACHmode</i>"</code>. For example <code>"EXE32"</code> or <code>"DYLIB64"</code>.
+     * @returns {String}
+     */
+MACH.getGeneralOptions = function() {}
+    /**
+     * Get the current version of a library.
+     * @param {String} sLibrary - The name of the library.
+     * @returns {String}
+     */
+MACH.getLibraryCurrentVersion = function(sLibrary) {}
+    /**
+     * Get the number of sections.
+     * @returns {Int}
+     */
+MACH.getNumberOfSections = function() {}
+    /**
+     * Get the number of segments.
+     * @returns {Int}
+     */
+MACH.getNumberOfSegments = function() {}
+    /**
+     * Check if there is a library with a specific name in the import table.
+     * @param {String} sLibraryName - The name of the library.
+     * @returns {Bool}
+     */
+MACH.isLibraryPresent = function(sLibrary) {}
+    /**
+     * Check if a section exists with a specific name or matches a regular expression.
+     * @param {String} sSectionName - Section name or pattern.
+     * @returns {Bool}
+     */
+MACH.isSectionNamePresent = function(sSectionName) {}
+    /**
+     * Get the number of a section with a specific name.
+     * @param {String} sSectionName - Section name.
+     * @returns {Int} 0-based section number, or <code>-1</code> if there is no section with that name.
+     */
+MACH.getSectionNumber = function(sSectionName) {}
+    /**
+     * Get the file offset of a section.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSectionFileOffset = function(nSection) {}
+    /**
+     * Get the file size of a section.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSectionFileSize = function(nSection) {}
+    /**
+     * Get the name of a section.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSectionName = function(nSection) {}
+    /**
+     * Get the file offset of a segment.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSegmentFileOffset = function(nSection) {}
+    /**
+     * Get the file size of a segment.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSegmentFileSize = function(nSection) {}
+    /**
+     * Get the name of a segment.
+     * @param {Int} nSection - Section number.
+     * @returns {UInt}
+     */
+MACH.getSegmentName = function(nSection) {}
+    /**
+     * Check if the file is 64 bit (MachO64).
+     * @returns {Bool}
+     * @example
+     * if(MACH.is64())
+     * {
+     *     sOptions="MachO64";
+     * }
+     */
+MACH.is64 = function() {}
+    /**
+     * Convert a relative virtual address (RVA) to a file offset.
+     * @param {UInt64} nRVA
+     * @returns {Int64} If an error occurs, -1 will be returned.
+     */
+MACH.RVAToOffset = function(nRVA) {}
+    /**
+     * Convert a virtual address (VA) to a file offset.
+     * @param {UInt64} nVA
+     * @returns {Int64} If an error occurs, -1 will be returned.
+     */
+MACH.VAToOffset = function(nVA) {}
+    /**
+     * Convert a file offset to a relative virtual address (RVA).
+     * @param {UInt64} nOffset
+     * @returns {Int64} If an error occurs, -1 will be returned.
+     */
+MACH.OffsetToRVA = function(nOffset) {}
+    /**
+     * Convert a file offset to a virtual address (VA).
+     * @param {UInt64} nOffset
+     * @returns {Int64} If an error occurs, -1 will be returned.
+     */
+MACH.OffsetToVA = function(nOffset) {}
+    /**
+     * Get the size of the overlay.
+     * @returns {Int}
+     */
+MACH.getOverlaySize = function() {}
+    /**
+     * Get the file offset of the overlay.
+     * @returns {Int}
+     */
+MACH.getOverlayOffset = function() {}
+    /**
+     * Check if an overlay is present.
+     * @returns {Bool}
+     */
+MACH.isOverlayPresent = function() {}
+    /**
+     * @see PE.compareOverlay
+     */
+MACH.compareOverlay = function(sSignature, nOffset) {}
+    /**
+     * Get the offset of the entry point.
+     * @returns {Int64} If an error occurs, -1 will be returned.
+     */
+MACH.getEntryPointOffset = function() {}
+    /**
+     * @see Binary.getFileBaseName
+     */
+MACH.getFileBaseName = function() {}
+    /**
+     * @see Binary.getFileSuffix
+     */
+MACH.getFileSuffix = function() {}
+    /**
+     * @see Binary.getFileCompleteSuffix
+     */
+MACH.getFileCompleteSuffix = function() {}
--- help/MSDOS.js
+++ help/MSDOS.js
@@ -2,147 +2,147 @@
  * @class
  * @classdesc This is a description of the MSDOS class.
  */
-function MSDOS(){}
+function MSDOS() {}
 /**
  * @see Binary.calculateEntropy
  */
-MSDOS.calculateEntropy=function(nOffset,nSize){}
-/**
- * @see Binary.calculateMD5
- */
-MSDOS.calculateMD5=function(nOffset,nSize){}
-/**
- * @see Binary.compare
- */
-MSDOS.compare=function(sSignature,nOffset){}
-/**
-  * @see PE.compareEP
- */
-MSDOS.compareEP=function(sSignature,nOffset){}
-/**
- * @see Binary.findByte
- */
-MSDOS.findByte=function(nOffset,nSize,cValue){}
-/**
- * @see Binary.findDword
- */
-MSDOS.findDword=function(nOffset,nSize,nValue){}
-/**
- * @see Binary.findSignature
- */
-MSDOS.findSignature=function(nOffset,nSize,sSignature){}
-/**
- * @see Binary.findString
- */
-MSDOS.findString=function(nOffset,nSize,sValue){}
-/**
- * @see Binary.findWord
- */
-MSDOS.findWord=function(nOffset,nSize,sValue){}
-/**
- * @see Binary.getSignature
- */
-MSDOS.getSignature=function(nOffset,nSize){}
-/**
- * @see Binary.getSize
- */
-MSDOS.getSize=function(){}
-/**
- * @see Binary.isSignaturePresent
- */
-MSDOS.isSignaturePresent=function(nOffset,nSize,sSignature){}
-/**
- * @see Binary.readByte
- */
-MSDOS.readByte=function(nOffset){}
-/**
- * @see Binary.readDword
- */
-MSDOS.readDword=function(nOffset){}
-/**
- * @see Binary.readQword
- */
-MSDOS.readQword=function(nOffset){}
-/**
- * @see Binary.readWord
- */
-MSDOS.readWord=function(nOffset){}
-/**
- * @see Binary.readSByte
- */
-MSDOS.readSByte=function(nOffset){}
-/**
- * @see Binary.readSDword
- */
-MSDOS.readSDword=function(nOffset){}
-/**
- * @see Binary.readSQword
- */
-MSDOS.readSQword=function(nOffset){}
-/**
- * @see Binary.readSWord
- */
-MSDOS.readSWord=function(nOffset){}
-/**
- * @see Binary.swapBytes
- */
-MSDOS.swapBytes=function(nValue){}
-/**
- * @see Binary.getString
- */
-MSDOS.getString=function(nOffset,nSize){}
-/**
- * @see Binary.getFileDirectory
- */
-MSDOS.getFileDirectory=function(){}
-/**
- * Get the file offset of the overlay.
- * @returns {Int}
- */
-MSDOS.getOverlayOffset=function(){}
-/**
- * Get the size of the overlay.
- * @returns {Int}
- */
-MSDOS.getOverlaySize=function(){}
-/**
- * Check if an overlay is present.
- * @returns {Bool}
- */
-MSDOS.isOverlayPresent=function(){}
-/**
- * Check if the file is a <b>N</b>ew <b>E</b>xecutable.
- * @returns {Bool}
- */
-MSDOS.isNE=function(){}
-/**
- * Check if the file is a <b>L</b>inear <b>E</b>xecutable.
- * @returns {Bool}
- */
-MSDOS.isLE=function(){}
-/**
- * Check if the file is a <b>L</b>inear e<b>X</b>ecutable.
- * @returns {Bool}
- */
-MSDOS.isLX=function(){}
-/**
- * @see PE.compareOverlay
- */
-MSDOS.compareOverlay=function(sSignature,nOffset){}
-/**
- * Get the offset of the entry point.
- * @returns {Int} If an error occurs, -1 will be returned.
- */
-MSDOS.getEntryPointOffset=function(){}
-/**
- * @see Binary.getFileBaseName
- */
-MSDOS.getFileBaseName=function(){}
-/**
- * @see Binary.getFileSuffix
- */
-MSDOS.getFileSuffix=function(){}
-/**
- * @see Binary.getFileCompleteSuffix
- */
-MSDOS.getFileCompleteSuffix=function(){}
+MSDOS.calculateEntropy = function(nOffset, nSize) {}
+    /**
+     * @see Binary.calculateMD5
+     */
+MSDOS.calculateMD5 = function(nOffset, nSize) {}
+    /**
+     * @see Binary.compare
+     */
+MSDOS.compare = function(sSignature, nOffset) {}
+    /**
+     * @see PE.compareEP
+     */
+MSDOS.compareEP = function(sSignature, nOffset) {}
+    /**
+     * @see Binary.findByte
+     */
+MSDOS.findByte = function(nOffset, nSize, cValue) {}
+    /**
+     * @see Binary.findDword
+     */
+MSDOS.findDword = function(nOffset, nSize, nValue) {}
+    /**
+     * @see Binary.findSignature
+     */
+MSDOS.findSignature = function(nOffset, nSize, sSignature) {}
+    /**
+     * @see Binary.findString
+     */
+MSDOS.findString = function(nOffset, nSize, sValue) {}
+    /**
+     * @see Binary.findWord
+     */
+MSDOS.findWord = function(nOffset, nSize, sValue) {}
+    /**
+     * @see Binary.getSignature
+     */
+MSDOS.getSignature = function(nOffset, nSize) {}
+    /**
+     * @see Binary.getSize
+     */
+MSDOS.getSize = function() {}
+    /**
+     * @see Binary.isSignaturePresent
+     */
+MSDOS.isSignaturePresent = function(nOffset, nSize, sSignature) {}
+    /**
+     * @see Binary.readByte
+     */
+MSDOS.readByte = function(nOffset) {}
+    /**
+     * @see Binary.readDword
+     */
+MSDOS.readDword = function(nOffset) {}
+    /**
+     * @see Binary.readQword
+     */
+MSDOS.readQword = function(nOffset) {}
+    /**
+     * @see Binary.readWord
+     */
+MSDOS.readWord = function(nOffset) {}
+    /**
+     * @see Binary.readSByte
+     */
+MSDOS.readSByte = function(nOffset) {}
+    /**
+     * @see Binary.readSDword
+     */
+MSDOS.readSDword = function(nOffset) {}
+    /**
+     * @see Binary.readSQword
+     */
+MSDOS.readSQword = function(nOffset) {}
+    /**
+     * @see Binary.readSWord
+     */
+MSDOS.readSWord = function(nOffset) {}
+    /**
+     * @see Binary.swapBytes
+     */
+MSDOS.swapBytes = function(nValue) {}
+    /**
+     * @see Binary.getString
+     */
+MSDOS.getString = function(nOffset, nSize) {}
+    /**
+     * @see Binary.getFileDirectory
+     */
+MSDOS.getFileDirectory = function() {}
+    /**
+     * Get the file offset of the overlay.
+     * @returns {Int}
+     */
+MSDOS.getOverlayOffset = function() {}
+    /**
+     * Get the size of the overlay.
+     * @returns {Int}
+     */
+MSDOS.getOverlaySize = function() {}
+    /**
+     * Check if an overlay is present.
+     * @returns {Bool}
+     */
+MSDOS.isOverlayPresent = function() {}
+    /**
+     * Check if the file is a <b>N</b>ew <b>E</b>xecutable.
+     * @returns {Bool}
+     */
+MSDOS.isNE = function() {}
+    /**
+     * Check if the file is a <b>L</b>inear <b>E</b>xecutable.
+     * @returns {Bool}
+     */
+MSDOS.isLE = function() {}
+    /**
+     * Check if the file is a <b>L</b>inear e<b>X</b>ecutable.
+     * @returns {Bool}
+     */
+MSDOS.isLX = function() {}
+    /**
+     * @see PE.compareOverlay
+     */
+MSDOS.compareOverlay = function(sSignature, nOffset) {}
+    /**
+     * Get the offset of the entry point.
+     * @returns {Int} If an error occurs, -1 will be returned.
+     */
+MSDOS.getEntryPointOffset = function() {}
+    /**
+     * @see Binary.getFileBaseName
+     */
+MSDOS.getFileBaseName = function() {}
+    /**
+     * @see Binary.getFileSuffix
+     */
+MSDOS.getFileSuffix = function() {}
+    /**
+     * @see Binary.getFileCompleteSuffix
+     */
+MSDOS.getFileCompleteSuffix = function() {}
